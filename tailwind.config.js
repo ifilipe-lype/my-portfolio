0d13481b9e18739b081f35c2d937c627b7e5018a
ifilipe.lype <filipe7lype@gmail.com>
2021-06-19 12:39:14 +0100

    extends container props

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1rem",
+          lg: "0"
+        },
+      }
+    },
   },
   variants: {
     extend: {},
