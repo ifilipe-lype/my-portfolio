1ef7165bebbdf8dbddc82f61ead4cb66e6e0dbd5
ifilipe.lype <filipe7lype@gmail.com>
2021-06-19 23:39:01 +0100

    adds z-index prop and set cta button to dark-gray

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
-    <header className="shadow-sm">
+    <header className="shadow-sm sticky top-0 bg-white z-50">
       <div className="container py-4 max-w-screen-lg">
         <nav className="flex md:flex-row flex-col md:items-center justify-between">
           <div className="flex justify-between items-center">
@@ -74,7 +74,7 @@ export default function Header() {
               </li>
             </ul>
 
-            <button className="px-4 md:px-6 py-2 w-full md:w-auto mt-4 md:mt-0 bg-indigo-500 hover:bg-indigo-600 transition text-white shadow md:text-sm rounded-xl focus:outline-none">
+            <button className="px-4 md:px-6 py-2 w-full md:w-auto mt-4 md:mt-0 bg-gray-700 hover:bg-gray-900 transition text-white shadow md:text-sm rounded-xl focus:outline-none">
               Hire me
             </button>
 
