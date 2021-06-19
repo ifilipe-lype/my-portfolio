16c5bf971b6128378d4cd77803ed67781716d3b7
ifilipe.lype <filipe7lype@gmail.com>
2021-06-19 23:45:23 +0100

    first attemp on hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Image from "next/image";
+
+import codingImageSrc from "../public/undraw_webdev.svg"
 
 import Header from '../components/header';
 
@@ -13,6 +16,30 @@ export default function Home() {
 
       <main className="">
         <Header />
+        <section className="">
+          <div className="container py-8 lg:py-16 items-centerx flex md:flex-row flex-col-reverse max-w-screen-lg md:justify-between">
+            <article className="md:w-1/2 transform md:transform-none py-4 -translate-y-32 z-10 bg-white bg-opacity-50 lg:bg-opacity-0 text-gray-800 xmt-8 md:mt-0 md:pr-4 text-center lg:text-left">
+              <h4 className="text-lg">Hello, world!</h4>
+              <h1 className="lg:text-5xl text-indigo-600 text-3xl font-semibold">I'm Lype</h1>
+              <h3 className="text-2xl lg:text-4xl font-semibold">Software Developer</h3>
+              <p className="py-4 text-lg">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero neque incidunt et tenetur placeat unde facilis nostrum at exercitationem.
+              </p>
+              <div className="flex lg:flex-row text-center flex-col items-center mt-8">
+                <span className="mb-4 lg:mb-0 w-full px-4 py-2 md:px-6 md:py-3 lg:mr-8 bg-gray-900 text-white rounded-lg">
+                  Hire me
+                </span>
+
+                <span className="w-full px-4 py-2 md:px-6 md:py-3 border border-gray-200 text-gray-700 rounded-lg">
+                  See what I got for you
+                </span>
+              </div>
+            </article>
+            <div className="relative z-0 md:w-1/2">
+              <Image src={codingImageSrc} alt="Illustration of a programmer coding..." />
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   )
