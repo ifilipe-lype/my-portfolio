67d100de0cc5043d338ff47b980bc4671423942f
ifilipe.lype <filipe7lype@gmail.com>
2021-06-19 23:48:30 +0100

    adds font configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
       container: {
         center: true,
         padding: {
