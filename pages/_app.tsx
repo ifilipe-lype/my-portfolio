c61e53fdd4789fcb1907cb1fe675149426895243
ifilipe.lype <filipe7lype@gmail.com>
2021-06-20 21:01:53 +0100

    moves fonts links to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,10 @@
-import { ThemeProvider } from 'next-themes'
-import Head from 'next/head'
+import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider defaultTheme="light" attribute="class">
-        <Head>
-          <meta name="description" content="An image uploader app" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
