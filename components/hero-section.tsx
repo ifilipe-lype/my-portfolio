988da8f2d5a34c4e90d43e86ad2f0d1c7a41cbdb
ifilipe.lype <filipe7lype@gmail.com>
2021-06-23 15:03:45 +0100

    done with hero section

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,37 +1,39 @@
-import Image from 'next/image';
-import codingImageSrc from '../public/undraw_webdev.svg';
+import Link from 'next/link';
 
-export default function HeroSection(){
-    return (
-        <section className="">
-          <div className="container py-8 lg:py-16 flex md:flex-row flex-col-reverse max-w-screen-lg md:justify-between">
-            <article className="md:w-1/2 transform md:transform-none py-4 -translate-y-32 z-10 bg-white dark:bg-transparent bg-opacity-50 lg:bg-opacity-0 text-gray-800 dark:text-white md:mt-0 md:pr-4 text-center lg:text-left">
-              <h4 className="text-lg text-gray-600 dark:text-gray-200 mb-1">Hello, world!</h4>
-              <h1 className="lg:text-5xl text-3xl">I&apos;m Lype</h1>
-              <h3 className="text-2xl text-indigo-500 font-semibold lg:text-4xl">
-                Software Developer
-              </h3>
-              <p className="py-4 text-lg">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
-                neque incidunt et tenetur placeat unde facilis nostrum at
-                exercitationem.
-              </p>
-              <div className="flex lg:flex-row text-center flex-col-reverse justify-start items-center mt-8">
-                <span className="w-full transition md:w-auto shadow px-4 py-2 md:px-6 md:py-3 bg-gray-700 hover:bg-gray-800 text-gray-100 rounded-lg">
-                  See my works
-                </span>
-                <span className="mb-4 lg:mb-0 transition w-full md:w-auto px-4 py-2 md:px-6 md:py-3 lg:ml-8 bg-indigo-500 hover:bg-indigo-600 text-white rounded-lg">
-                  Hire me
-                </span>
-              </div>
-            </article>
-            <div className="relative z-0 md:w-1/2">
-              <Image
-                src={codingImageSrc}
-                alt="Illustration of a programmer coding..."
-              />
-            </div>
+export default function HeroSection() {
+  return (
+    <section className="">
+      <div className="container min-h-screen relative flex justify-center lg:justify-start items-center max-w-screen-lg">
+        <div
+          className="w-full lg:w-8/12 h-full bg-cover bg-no-repeat bg-top absolute right-0"
+          style={{
+            backgroundImage: `url(/avatar.jpg)`,
+          }}
+        ></div>
+        <div className="w-full lg:w-8/12 h-full bg-gray-900 bg-opacity-40 absolute right-0"></div>
+        <article className="md:w-1/2 rounded-lg z-10 text-white lg:text-gray-600 dark:text-white text-center lg:text-left">
+          <h4 className="text-lg mb-1">Hello, world! I&apos;m</h4>
+          <h1 className="lg:text-5xl text-3xl mb-2">Filipe André</h1>
+          <h3 className="text-2xl text-purple-500 font-semibold lg:text-4xl">
+            Software Developer
+          </h3>
+          <div className="block my-2">
+            <p className="text-base lg:p-4 lg:pl-0 lg:bg-white lg:dark:bg-gray-900 rounded-lg">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
+              neque incidunt et tenetur placeat unde facilis nostrum at
+              exercitationem.
+            </p>
           </div>
-        </section>
-    )
+          <div className="flex w-9/12 mx-auto lg:w-full lg:mx-0 md:flex-row text-center flex-col items-center mt-8">
+            <span className="mb-4 md:mb-0 transition w-full lg:w-auto px-4 py-2 lg:px-6 lg:py-3 md:mr-8 bg-purple-500 hover:bg-purple-600 text-white rounded-lg">
+              Hire me
+            </span>
+            <span className="w-full transition cursor-pointer lg:w-auto shadow px-4 py-2 lg:px-6 lg:py-3 bg-gray-900 hover:bg-gray-700 text-gray-100 rounded-lg">
+              See my works
+            </span>
+          </div>
+        </article>
+      </div>
+    </section>
+  )
 }
