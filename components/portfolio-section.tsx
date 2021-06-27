fadf63e2659c12b49ee021a92b2656c2145fc8a7
ifilipe.lype <filipe7lype@gmail.com>
2021-06-27 14:38:05 +0100

    changes section header's title and adds key props to map items

--- components/portfolio-section.tsx
+++ components/portfolio-section.tsx
@@ -63,15 +63,15 @@ export default function PortfolioSection() {
     >
       <div className="container max-w-screen-lg flex flex-col justify-center mb-4 items-center">
         <header className="flex flex-col dark:text-white justify-center items-center text-center">
-          <h1 className="text-2xl lg:text-3xl uppercase mb-2">Portfolio</h1>
+          <h1 className="text-xl md:text-2xl lg:text-3xl uppercase mb-2">Portfolio</h1>
           <h4 className="lg:text-lg">See what I&apos;ve been doing lately</h4>
           <div className="h-1 w-5/12 rounded-full bg-purple-500 mt-2"></div>
         </header>
 
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 py-10 lg:py-20">
           {
-            mockedProjectList.map(project => (
-              <div className="" key={project.name}>
+            mockedProjectList.map((project, index) => (
+              <div className="" key={index}>
                 <ProjectCard {...project} />
               </div>
             ))
