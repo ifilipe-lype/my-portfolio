60ac80592f3c9f88ef262076de3cac7a1bb27c61
ifilipe.lype <filipe7lype@gmail.com>
2021-07-03 23:32:16 +0100

    adds animation

--- components/skills-section.tsx
+++ components/skills-section.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import Image from 'next/image';
+import { motion } from "framer-motion";
 
 interface Skill {
     logo: string;
@@ -37,31 +38,57 @@ const skills: Skill[] = [
     { logo: "/icons/github.png", name: "Github"},
 ]
 
+const skillsListVariants = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      type: 'spring', // default
+      mass: 0.4, // the weight of the component
+      damping: 8, // the bounce back-and-forth
+      when: 'beforeChildren',
+      staggerChildren: 0.3,
+    }
+  }
+}
+
+const skillItemVariants = {
+  initial: { opacity: 0, scale: .8 },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+    }
+  }
+}
+
 const SkillSection: FC = () => {
   return (
     <section className="py-16 lg:py-24 bg-purple-900">
       <div className="container max-w-screen-lg flex flex-col justify-center mb-10 md:mb-16 items-center">
-        <header className="flex flex-col text-white justify-center items-center text-center">
+        <motion.header initial={{y: "100vh"}} animate={{ y: 0}} className="flex flex-col text-white justify-center items-center text-center">
           <h1 className="text-xl md:text-2xl lg:text-3xl uppercase mb-2">
             My Skills
           </h1>
           <h4 className="lg:text-lg">
-            See what tools I use to bring excelents solution alive
+            See some of the tools I use to bring excelents solution alive
           </h4>
           <div className="h-1 w-5/12 rounded-full bg-white mt-2"></div>
-        </header>
+        </motion.header>
       </div>
 
       <div className="container max-w-screen-lg">
-        <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        <motion.ul variants={skillsListVariants} animate="animate" initial="initial"
+          className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
           {
               skills.map((skill, index) => (
-                  <li key={index}>
+                  <motion.li variants={skillItemVariants} key={index}>
                       <SkillCard {...skill} />
-                  </li>
+                  </motion.li>
               ))
           }
-        </ul>
+        </motion.ul>
       </div>
     </section>
   )
