776091e2faf34128686287f4d8ed232779bf1dbd
ifilipe.lype <filipe7lype@gmail.com>
2021-07-07 09:55:24 +0100

    extracts SkillCard into a file, and imports it

--- components/skills-section.tsx
+++ components/skills-section.tsx
@@ -1,30 +1,9 @@
 import { FC, useEffect, useState } from 'react'
-import Image from 'next/image'
 import { motion } from 'framer-motion'
 
 import Loading from "./loading"
+import SkillCard from './skill-card'
 
-interface Skill {
-  logo: string
-  name: string
-}
-
-const SkillCard: FC<Skill> = ({ logo, name }) => {
-  return (
-    <article className="flex flex-col items-center justify-center">
-      <div className="relative w-16 h-16 md:w-20 md:h-20">
-        <Image
-          src={logo}
-          sizes="100%"
-          layout="fill"
-          objectFit="contain"
-          alt={name}
-        />
-      </div>
-      <h5 className="text-sm md:text-lg text-white">{name}</h5>
-    </article>
-  )
-}
 
 const skillsListVariants = {
   initial: { opacity: 0 },
