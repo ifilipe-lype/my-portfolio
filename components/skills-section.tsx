a1c8870bc8400973807af584aaa99f68df539435
ifilipe.lype <filipe7lype@gmail.com>
2021-07-07 09:50:12 +0100

    displays a loading, while fetching skills data from api

--- components/skills-section.tsx
+++ components/skills-section.tsx
@@ -1,10 +1,12 @@
-import { FC } from 'react'
-import Image from 'next/image';
-import { motion } from "framer-motion";
+import { FC, useEffect, useState } from 'react'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
+
+import Loading from "./loading"
 
 interface Skill {
-    logo: string;
-    name: string;
+  logo: string
+  name: string
 }
 
 const SkillCard: FC<Skill> = ({ logo, name }) => {
@@ -24,21 +26,6 @@ const SkillCard: FC<Skill> = ({ logo, name }) => {
   )
 }
 
-const skills: Skill[] = [
-    { logo: "/icons/html-5.png", name: "HTML5"},
-    { logo: "/icons/css-3.png", name: "CSS3"},
-    { logo: "/icons/js.png", name: "JavaScript"},
-    { logo: "/icons/tailwindcss.png", name: "Tailwindcss"},
-    { logo: "/icons/reactjs.png", name: "ReactJS"},
-    { logo: "/icons/nextjs.png", name: "NextJS"},
-    { logo: "/icons/ts.png", name: "TypeScript"},
-    { logo: "/icons/nodejs.png", name: "NodeJS"},
-    { logo: "/icons/mongodb.png", name: "MongoDB"},
-    { logo: "/icons/git.png", name: "Git"},
-    { logo: "/icons/github.png", name: "Github"},
-    { logo: "/icons/jest.png", name: "TDD, Jest"},
-]
-
 const skillsListVariants = {
   initial: { opacity: 0 },
   animate: {
@@ -49,26 +36,40 @@ const skillsListVariants = {
       damping: 8, // the bounce back-and-forth
       when: 'beforeChildren',
       staggerChildren: 0.25,
-    }
-  }
+    },
+  },
 }
 
 const skillItemVariants = {
-  initial: { opacity: 0, scale: .8 },
+  initial: { opacity: 0, scale: 0.8 },
   animate: {
     opacity: 1,
     scale: 1,
     transition: {
-      type: "spring",
-    }
-  }
+      type: 'spring',
+    },
+  },
 }
 
 const SkillSection: FC = () => {
+  const [skills, setSkills] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api/skills");
+      const data = await response.json()
+      setSkills(data);
+    })()
+  }, [])
+
   return (
     <section id="skills" className="py-16 lg:py-24 bg-purple-900">
       <div className="container max-w-screen-lg flex flex-col justify-center mb-10 md:mb-16 items-center">
-        <motion.header initial={{y: "100vh"}} animate={{ y: 0}} className="flex flex-col text-white justify-center items-center text-center">
+        <motion.header
+          initial={{ y: '100vh' }}
+          animate={{ y: 0 }}
+          className="flex flex-col text-white justify-center items-center text-center"
+        >
           <h1 className="text-xl md:text-2xl lg:text-3xl uppercase mb-2">
             My Skills
           </h1>
@@ -79,17 +80,25 @@ const SkillSection: FC = () => {
         </motion.header>
       </div>
 
-      <div className="container max-w-screen-lg">
-        <motion.ul variants={skillsListVariants} animate="animate" initial="initial"
-          className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
-          {
-              skills.map((skill, index) => (
-                  <motion.li variants={skillItemVariants} key={index}>
-                      <SkillCard {...skill} />
-                  </motion.li>
-              ))
-          }
-        </motion.ul>
+      <div className="container max-w-screen-lg flex flex-col justify-center items-center">
+        {skills.length ? (
+          <motion.ul
+            variants={skillsListVariants}
+            animate="animate"
+            initial="initial"
+            className="w-full grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6"
+          >
+            {skills.map((skill, index) => (
+              <motion.li variants={skillItemVariants} key={index}>
+                <SkillCard {...skill} />
+              </motion.li>
+            ))}
+          </motion.ul>
+        ) : (
+          <div className="mt-4">
+            <Loading className="text-purple-700 h-20 w-20" />
+          </div>
+        )}
       </div>
     </section>
   )
