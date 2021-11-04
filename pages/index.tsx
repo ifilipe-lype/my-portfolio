1c677293b97d2516a91f82048489fef2220bad34
ifilipe-lype <filipe7lype@gmail.com>
2021-11-04 23:16:13 +0100

    gets initial data from server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import Header from '../components/header'
-import HeroSection from '../components/hero-section'
-import PortfolioSection from '../components/portfolio-section'
-import SkillSection from '../components/skills-section'
-import AboutMeSection from '../components/about-section'
+import Header from "../components/header";
+import HeroSection from "../components/hero-section";
+import PortfolioSection from "../components/portfolio-section";
+import SkillSection from "../components/skills-section";
+import AboutMeSection from "../components/about-section";
 
-export default function Home() {
+import { Project, Skill } from "../@types";
+import { getProjects, getSkills } from "../lib/datocms";
+import { FC } from "react";
+
+interface HomeInitProps {
+  projects: Project[];
+  skills: Skill[];
+}
+
+const Home: FC<HomeInitProps> = ({ projects, skills }) => {
   return (
     <div id="home">
       <Head>
@@ -16,10 +25,26 @@ export default function Home() {
       <main className="">
         <Header />
         <HeroSection />
-        <PortfolioSection />
-        <SkillSection />
+        <PortfolioSection projects={projects} />
+        <SkillSection skills={skills} />
         <AboutMeSection />
       </main>
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps() {
+  const [projects, skills] = await Promise.all([
+    await getProjects(),
+    await getSkills(),
+  ]);
+
+  return {
+    props: {
+      projects,
+      skills,
+    },
+  };
 }
+
+export default Home;
