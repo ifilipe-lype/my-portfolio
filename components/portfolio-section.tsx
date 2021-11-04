488d490decd48f0f9ce51497a0d67b4644a195fc
ifilipe-lype <filipe7lype@gmail.com>
2021-11-04 23:22:16 +0100

    remove unnecessary imports

--- components/portfolio-section.tsx
+++ components/portfolio-section.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 
 import ProjectCard from "./projectCard";
 import Loading from "./loading";
